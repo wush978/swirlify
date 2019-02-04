70fa02398a87a3c1ecfcf39b8b26473384a24de3
Wush Wu <wush978@gmail.com>
2019-02-04 09:32:03 +0800

    debug: repos not found

--- inst/test-script/test.R
+++ inst/test-script/test.R
@@ -9,18 +9,16 @@ Sys.setenv(R_LIBS=paste(R_LIBS, collapse = ":"))
 utils::install.packages("remotes", repos = "http://cran.csie.ntu.edu.tw", lib = R_LIBS[1])
 utils::install.packages("pvm", repos = NULL, lib = R_LIBS[1])
 pvm::import.packages(sprintf("https://raw.githubusercontent.com/wush978/pvm-list/master/dsr-%s.yml", package_version(R.version)), lib = R_LIBS[2])
-if (!suppressWarnings(require(subprocess))) install.packages("subprocess", repos = repos, lib = R_LIBS[2])
-if (!suppressWarnings(require(magrittr))) install.packages("magrittr", repos = repos, lib = R_LIBS[2])
 utils::install.packages("swirl", repos = NULL, lib = R_LIBS[1])
-utils::install.packages("swirlify", repos = NULL, lib = R_LIBS[1])
-Sys.setenv("SWIRL_DEV"="TRUE")
-library(swirl)
 
 R.date <- pvm::R.release.dates[R.version]
 R_USER_LIBS <- file.path("R-lib", TEST_COURSE, LESSON_PREFIX, R.version, R.date)
 dir.create(R_USER_LIBS, showWarnings = FALSE)
 repos <- c(CRAN = sprintf("https://cran.microsoft.com/snapshot/%s", R.date + 7))
+if (!suppressWarnings(require(subprocess))) install.packages("subprocess", repos = repos, lib = R_LIBS[2])
+if (!suppressWarnings(require(magrittr))) install.packages("magrittr", repos = repos, lib = R_LIBS[2])
+utils::install.packages("swirlify", repos = NULL, lib = R_LIBS[1])
 
+Sys.setenv("SWIRL_DEV"="TRUE")
 .libPaths(new = c(R_USER_LIBS, .libPaths()))
-
 swirlify::test_lesson_by_agent(file.path("course", TEST_COURSE), LESSON_PREFIX, repos)
