d9ef96a2ec421da0fd59ef1dc2effd0b6ce85891
Wush Wu <wush978@gmail.com>
2019-02-05 01:08:32 +0800

    for windows

--- inst/test-script/test.R
+++ inst/test-script/test.R
@@ -1,10 +1,14 @@
 R.version <- sprintf("%s.%s", R.version$major, R.version$minor)
 LESSON_PREFIX <- Sys.getenv("LESSON_PREFIX")
 TEST_COURSE <- Sys.getenv("TEST_COURSE")
-R_LIBS <- c(tempfile(), file.path("R-lib", R.version))
+if (Sys.info()["sysname"] == "Windows") {
+  R_LIBS <- c(tempfile(), normalizePath(file.path("c:/workspace/R-lib", R.version)))
+} else {
+  R_LIBS <- c(tempfile(), file.path("R-lib", R.version))
+}
 lapply(R_LIBS, dir.create, recursive = TRUE, showWarnings = FALSE)
 .libPaths(new = R_LIBS)
-Sys.setenv(R_LIBS=paste(R_LIBS, collapse = ":"))
+Sys.setenv("R_LIBS" = paste(R_LIBS, collapse = ":"))
 
 utils::install.packages("remotes", repos = "http://cran.csie.ntu.edu.tw", lib = R_LIBS[1])
 utils::install.packages("pvm", repos = NULL, lib = R_LIBS[1])
@@ -12,8 +16,13 @@ pvm::import.packages(sprintf("https://raw.githubusercontent.com/wush978/pvm-list
 utils::install.packages("swirl", repos = NULL, lib = R_LIBS[1])
 
 R.date <- pvm::R.release.dates[R.version]
-R_USER_LIBS <- file.path("R-lib", TEST_COURSE, LESSON_PREFIX, R.version, R.date)
-dir.create(R_USER_LIBS, showWarnings = FALSE)
+
+if (Sys.info()["sysname"] == "Windows") {
+  R_USER_LIBS <- normalizePath(file.path("c:/workspace/R-lib", TEST_COURSE, LESSON_PREFIX, R.version, R.date))
+} else {
+  R_USER_LIBS <- file.path("R-lib", TEST_COURSE, LESSON_PREFIX, R.version, R.date)
+}
+dir.create(R_USER_LIBS, showWarnings = FALSE, recursive = TRUE)
 repos <- c(CRAN = sprintf("https://cran.microsoft.com/snapshot/%s", R.date + 7))
 if (!suppressWarnings(require(subprocess))) install.packages("subprocess", repos = repos, lib = R_LIBS[2])
 if (!suppressWarnings(require(magrittr))) install.packages("magrittr", repos = repos, lib = R_LIBS[2])
